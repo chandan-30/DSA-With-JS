0526e4856443939e0fefb74d21b834a1a4cb543e
Sai Chandan <saichandhan.yata44@gmail.com>
2023-10-15 11:07:19 +0530

    Added comments

--- Linked-Lists/linked-list-implementation.js
+++ Linked-Lists/linked-list-implementation.js
@@ -1,12 +1,12 @@
 class Node {
-    constructor( value ) {
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
 
 class LinkedList {
-    constructor( value ) {
+    constructor(value) {
         this.head = {
             value,
             next: null,
@@ -15,75 +15,77 @@ class LinkedList {
         this.length = 1;
     }
 
-    _append( value ) {
-        if ( this.tail ) {
-            const newNode = new Node( value );
+    // Append a new node with the given value to the end of the linked list.
+    _append(value) {
+        if (this.tail) {
+            const newNode = new Node(value);
             this.tail.next = newNode;
             this.tail = newNode;
-            this.length = this.length + 1;
+            this.length++;
         }
     }
 
-    _prepend( value ) {
-        if( this.head ) {
-            const newNode = new Node( value );
+    // Prepend a new node with the given value to the beginning of the linked list.
+    _prepend(value) {
+        if (this.head) {
+            const newNode = new Node(value);
             newNode.next = this.head;
             this.head = newNode;
             this.length++;
         }
     }
 
+    // Print the values of all nodes in the linked list as an array.
     printList() {
         const arr = [];
         let currentNode = this.head;
-        while ( currentNode ) {
-            arr.push( currentNode.value );
+        while (currentNode) {
+            arr.push(currentNode.value);
             currentNode = currentNode.next;
         }
         return arr;
     }
 
-    _insert( index, value ) {
-        if ( index > this.length ) {
-            this._append( value );
+    // Insert a new node with the given value at the specified index in the linked list.
+    _insert(index, value) {
+        if (index > this.length) {
+            this._append(value);
             return;
         }
 
-        if ( index === 0 ) {
-            this._prepend( value );
+        if (index === 0) {
+            this._prepend(value);
             return;
         }
-        const newNode = new Node( value );
-        let nodeToMakeChanges = this._traverseToIndex( index - 1 );
+
+        const newNode = new Node(value);
+        let nodeToMakeChanges = this._traverseToIndex(index - 1);
         const tempNode = nodeToMakeChanges.next;
         nodeToMakeChanges.next = newNode;
         newNode.next = tempNode;
         this.length++;
     }
 
-    _traverseToIndex( index ) {
+    // Traverse to the node at the specified index in the linked list.
+    _traverseToIndex(index) {
         let counter = 0;
         let currentNode = this.head;
-        while ( counter !== index ) {
+        while (counter !== index) {
             currentNode = currentNode.next;
             counter++;
         }
         return currentNode;
     }
 
-    _remove( index ) {
-        if ( index === 0 ) {
+    // Remove the node at the specified index from the linked list.
+    _remove(index) {
+        if (index === 0) {
             const tempNode = this.head.next;
             this.head = tempNode;
             this.length--;
             return;
         }
-        let nodeToMakeChanges = this._traverseToIndex( index - 1 );
-        // if ( nodeToMakeChanges.next.next === null ) {
-        //     nodeToMakeChanges.next = null;
-        // } else {
-        //     nodeToMakeChanges.next = nodeToMakeChanges.next.next;
-        // }
+        let nodeToMakeChanges = this._traverseToIndex(index - 1);
         let tempNode = nodeToMakeChanges.next;
         nodeToMakeChanges.next = tempNode.next;
         this.length--;
@@ -91,14 +93,16 @@ class LinkedList {
     }
 }
 
-const myLinkedList = new LinkedList( 1 );
-myLinkedList._append( 2 );
-myLinkedList._prepend( 0 );
-myLinkedList._insert( 2, 1.5 );
-myLinkedList._insert( 5, 3 );
-myLinkedList._insert( 0, -1 );
-myLinkedList._remove( 0 );
-myLinkedList._remove( 2 );
-myLinkedList._remove( 3 );
-console.log( myLinkedList.length );
-console.log( myLinkedList.printList() );
+// Example usage:
+const myLinkedList = new LinkedList(1);
+myLinkedList._append(2);
+myLinkedList._prepend(0);
+myLinkedList._insert(2, 1.5);
+myLinkedList._insert(5, 3);
+myLinkedList._insert(0, -1);
+myLinkedList._remove(0);
+myLinkedList._remove(2);
+myLinkedList._remove(3);
+
+console.log(myLinkedList.length); // Output the length of the linked list.
+console.log(myLinkedList.printList()); // Output the values in the linked list.
