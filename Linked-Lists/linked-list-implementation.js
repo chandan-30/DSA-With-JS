43b388486030f7f5494c80a00affa1f2eb4dc2c0
Sai Chandan <saichandhan.yata44@gmail.com>
2023-10-15 11:05:21 +0530

    Insert and delete methods

--- Linked-Lists/linked-list-implementation.js
+++ Linked-Lists/linked-list-implementation.js
@@ -32,9 +32,73 @@ class LinkedList {
             this.length++;
         }
     }
+
+    printList() {
+        const arr = [];
+        let currentNode = this.head;
+        while ( currentNode ) {
+            arr.push( currentNode.value );
+            currentNode = currentNode.next;
+        }
+        return arr;
+    }
+
+    _insert( index, value ) {
+        if ( index > this.length ) {
+            this._append( value );
+            return;
+        }
+
+        if ( index === 0 ) {
+            this._prepend( value );
+            return;
+        }
+        const newNode = new Node( value );
+        let nodeToMakeChanges = this._traverseToIndex( index - 1 );
+        const tempNode = nodeToMakeChanges.next;
+        nodeToMakeChanges.next = newNode;
+        newNode.next = tempNode;
+        this.length++;
+    }
+
+    _traverseToIndex( index ) {
+        let counter = 0;
+        let currentNode = this.head;
+        while ( counter !== index ) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    _remove( index ) {
+        if ( index === 0 ) {
+            const tempNode = this.head.next;
+            this.head = tempNode;
+            this.length--;
+            return;
+        }
+        let nodeToMakeChanges = this._traverseToIndex( index - 1 );
+        // if ( nodeToMakeChanges.next.next === null ) {
+        //     nodeToMakeChanges.next = null;
+        // } else {
+        //     nodeToMakeChanges.next = nodeToMakeChanges.next.next;
+        // }
+        let tempNode = nodeToMakeChanges.next;
+        nodeToMakeChanges.next = tempNode.next;
+        this.length--;
+        return;
+    }
 }
 
 const myLinkedList = new LinkedList( 1 );
 myLinkedList._append( 2 );
 myLinkedList._prepend( 0 );
-console.log( myLinkedList );
+myLinkedList._insert( 2, 1.5 );
+myLinkedList._insert( 5, 3 );
+myLinkedList._insert( 0, -1 );
+myLinkedList._remove( 0 );
+myLinkedList._remove( 2 );
+myLinkedList._remove( 3 );
+console.log( myLinkedList.length );
+console.log( myLinkedList.printList() );
